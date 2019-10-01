ceaf7c4dbd271bbd69d7b1dd19d1e576bd304f1b
Lakshit <agglak2@gmail.com>
2019-10-01 20:34:54 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/array_sorting.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/array_sorting.dir/main.c.obj"
+  "array_sorting.exe"
+  "array_sorting.exe.manifest"
+  "array_sorting.pdb"
+  "libarray_sorting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/array_sorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
